99e35383fbb176daf28a1ec2f5f214818ccc0c4a
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-08-29 19:40:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
 
 app = pyrogram.Client(
-      "Siri",
+      "Alexa",
        bot_token=Config.BOT_TOKEN,
        api_id=Config.APP_ID,
        api_hash=Config.API_HASH,
