b2369ab4b0a66277d7c79dc4b46600f6e57a8271
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-08-29 19:44:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ app = pyrogram.Client(
        bot_token=Config.BOT_TOKEN,
        api_id=Config.APP_ID,
        api_hash=Config.API_HASH,
-       plugins=dict(root="siri")
+       plugins=dict(root="Alexa")
     )
 app.run()
