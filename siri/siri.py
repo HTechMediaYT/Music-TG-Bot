c765610a1522e016f329206c2e29f8a6d0feeba2
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-08-29 19:38:03 +0530

    Update siri.py

--- siri/siri.py
+++ siri/siri.py
@@ -89,7 +89,7 @@ def a(client, message):
         )
         print(str(e))
         return
-    m.edit("📣")
+    m.edit("💃🏻")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
