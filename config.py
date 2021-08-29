2ce2089905c4364aa709968b6451d2e427d31e7a
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-08-29 20:50:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
-    OWNER = os.environ.get("OWNER", "NxtStark") 
+    OWNER = os.environ.get("OWNER", "NxtStark")
+    CHAT_LINK = os.environ.get("CHAT_LINK", "")
     DOWNLOAD_LOCATION = os.environ.get("DOWNLOAD_LOCATION", "./DOWNLOADS/")
     msg = {}
